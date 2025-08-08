9dab9824a7dd0b0fd689f1b9baefecd76be67609
Nathaly Barbieri <ndelimabarbieri@gmail.com>
2025-08-07 21:24:51 -0300

    ADICIONANDO AS SEGUINTES FEATURES: AGORA O SISTEMA SÓ ACEITA NÚMEROS, CASO DETECTE UMA STRING, ELE A TRANSFORMA EM DETERMINADO NÚMERO DE ACORDO COM A OPERAÇÃO; O RESULTADO ÉXIBIDO NA CAIXA DE DIALOGO AO INVES DO CONSOLE. FOI CORRIGIDO O SEGUINTE BUG: O SISTEMA NÃO ESTAVA CONSEGUINDO CONVERTER UNDEFINED PARA INT PORQUE AO FAZER A CONVERSÃO NA FUNÇÃO, O NÚMERO REFERNCIAVA ELE MESMO E NÃO O VETOR GLOBAL QUE IRIA SER RETORNADO NA FUNÇÃO PRINCIPAL. DESSA FORMA, A POSIÇÃO DO VETOR GLOBAL RECEBIA O VALOR DA VETOR DA FUNÇÃO NA MESMA POSIÇÃO. TAMBÉM ALTEREI A FUNÇÃO DE ADICIONAR UM NOVO NÚMERO QUE SEMPRE PERGUNTAVA AO USUÁRIO SE ELE QUERIA E USAVA UMA STRING PARA DETERMINAR SE SIM OU SE NÃO (ESTAVA USANDO O WINDOW.PROMPT), DESSA FORMA, PARA DEIXAR O CÓDIGO MAIS LIMPO E EVITAR CARATERES QUE FUJAM DAS DUAS OPÇÕES DE MANEIRA MAIS LOGICA E ESTRATEGICA, MUDEI PARA O WINDOW.CONFIRM()

--- calculator.js
+++ calculator.js
@@ -1,104 +1,89 @@
-let num = [], numInt = [];
-
 function addAnotherNumber(){
     if(num.length <= 4){
-    let opContinue = window.prompt("Deseja adicionar mais um número?: ");
-    opContinue = opContinue.toUpperCase();
-        if(opContinue == "SIM" || opContinue ==  "S" || opContinue ==  "SS" ){
-            addNumber();
-        }
+        let opContinue = window.confirm("Deseja adicionar mais um número?");
+        if(opContinue == true){addNumber();}
     }
 }
+
 function addNumber(){
-    num[qtdNum] = window.prompt("Digite um número: ");
+    num[qtdNum] = window.prompt("Digite um número");
     qtdNum++;
     addAnotherNumber();
 }
 
-function tranformaInt(){
-    for(let i=0; i<num.length; i++){
-       numInt[i] = parseInt(num[i]);
+function tranformaInt(nums = [], cat){
+    let numstr = [];
+    cat = parseInt(cat);
+    numstr = nums;
+    for(let i=0; i<nums.length; i++){
+        numstr[i] = parseInt(numstr[i], 10);
+        if(!isNaN(numstr[i])){
+            numInt[i] = parseInt(nums[i]);
+        }else{
+            if(cat == 1 || cat == 2){numInt[i] = 0;}
+            else{numInt[i] = 1;}
+        }
     }
 }
 
-function transformaIndefined(cat){
+function transformaIndefined(cat, n3, n4, n5){
     cat = parseInt(cat);
-    console.log(cat);
     if(cat == 1 || cat == 2){
-        if(num[2] = undefined){
-            num[2] = 0;
-        }if(num[3] = undefined){
-            num[3] = 0;
-        }if(num[4] = undefined){
-            num[4] = 0;
-        }
+       if(n3 == undefined){num[2] = 0;}
+       if(n4 == undefined){num[3] = 0;}
+       if(n5 == undefined){num[4] = 0;}
     }else{
-        if(num[2] = undefined){
-            num[2] = 1;
-        }if(num[3] = undefined){
-            num[3] = 1;
-        }if(num[4] = undefined){
-            num[4] = 1;
-        }
+       if(n3 == undefined){num[2] = 1;}
+       if(n4 == undefined){num[3] = 1;}
+       if(n5 == undefined){num[4] = 1;}
     }
 }
 
 function somar(n1, n2, n3, n4, n5){
-    console.log((n1) + (n2) + (n3) + (n4) + (n5));
+    window.alert("O resultado é: " + ((n1) + (n2) + (n3) + (n4) + (n5)));
 }
 function subtrair(n1, n2, n3, n4, n5){
-    console.log(n1 - n2 - n3 - n4 - n5);
+     window.alert("O resultado é: " + ((n1) - (n2) - (n3) - (n4) - (n5)));
 }
 function dividir(n1, n2){
-    console.log(n1 / n2);
+     window.alert("O resultado é: " + (n1 / n2));
 }
 function multiplicar(n1, n2, n3, n4, n5){
-    console.log((n1) * (n2) * (n3) * (n4) * (n5));
+     window.alert("O resultado é: " + ((n1) * (n2) * (n3) * (n4) * (n5)));
 }
 function elevarPot(n1, n2){
-    console.log(Math.pow(n1, n2));
+     window.alert("O resultado é: " + (Math.pow(n1, n2)));
 }
 function raizQuadrada(n1){
-    console.log(Math.sqrt(n1));
+     window.alert("O resultado é: " + (Math.sqrt(n1)));
 }
 
+let num = [], numInt = [];
 let qtdNum = (num.length) - 1;
-
-let categoria = parseInt(window.prompt("Que tipo de operação você deseja fazer?\n\n1. Somar\n2. Subtrair\n3. Dividir\n4. Multiplicar\n5. Elevar ao quadrado\n6. Raiz Quadrada"));
+let categoria = parseInt(window.prompt("Que tipo de operação você deseja fazer?\n\n1. Somar\n2. Subtrair\n3. Dividir\n4. Multiplicar\n5. Potenciação\n6. Raiz Quadrada"));
 
 if(categoria == 1 || categoria == 2 || categoria == 4){
-    num[0] = window.prompt("Digite o primeiro número: ");
-    num[1] = window.prompt("Digite o segundo número: ");
+    num[0] = window.prompt("Digite o primeiro número");
+    num[1] = window.prompt("Digite o segundo número");
     qtdNum = 2;
     addAnotherNumber();
-    transformaIndefined(categoria);
-    tranformaInt();
-    console.log(numInt);
+    transformaIndefined(categoria, num[2], num[3], num[4]);
+    tranformaInt(num, categoria);
 
-    if(categoria == 1){
-        console.log(numInt[0], numInt[1], numInt[2], numInt[3], numInt[4]);
-        somar(numInt[0], numInt[1], numInt[2], numInt[3], numInt[4]);}
+    if(categoria == 1){somar(numInt[0], numInt[1], numInt[2], numInt[3], numInt[4]);}
     if(categoria == 2){subtrair(numInt[0], numInt[1], numInt[2], numInt[3], numInt[4]);}
     if(categoria == 4){multiplicar(numInt[0], numInt[1], numInt[2], numInt[3], numInt[4]);}   
-
 }
-
 else if(categoria == 3 || categoria == 5){
-    num[0] = window.prompt("Digite o primeiro número: ");
-    num[1] = window.prompt("Digite o segundo número: ");
-    tranformaInt();
-    if(categoria == 3){
-        dividir(numInt[0], numInt[1]);
-    }else{
-        elevarPot(numInt[0], numInt[1]);
-    }
+    num[0] = window.prompt("Digite o primeiro número");
+    num[1] = window.prompt("Digite o segundo número");
+    tranformaInt(num);
+
+    if(categoria == 3){dividir(numInt[0], numInt[1]);}
+    else{elevarPot(numInt[0], numInt[1]);}
 }
 else{
-    num[0] = window.prompt("Digite um número: ");
-    tranformaInt();
+    num[0] = window.prompt("Digite um número");
+    tranformaInt(num);
     raizQuadrada(numInt[0]);
 }
-
-
-
-
